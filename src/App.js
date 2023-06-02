0ed5fca74e857d0ba3c59b0d659300b98f731252
wanghexing <wanghexing@ly-sky.com>
2023-06-02 11:17:55 +0800

    timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import moment from 'moment'
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [flag, setflag] = useState(false);
+  const [count, setcount] = useState(0);
+  const [out, setout] = useState(false);
+  const intervalRef = useRef();
   useEffect(() => {
     setInterval(() => {
-      console.log('时间');
+      // console.log('时间');
       getTime()
     }, 200);
   }, []);
@@ -19,14 +23,43 @@ function App() {
     // console.log(res);
     res.then(data => {
       let time = moment(data.time).format('YYYY-MM-DD HH:mm:ss')
-      console.log(time);
+      // console.log(time);
       setCurrentTime(time)
     });
+  }
+  // 关闭
+  const stopatime = () => {
+    clearInterval(intervalRef.current);
+  }
+  const startatime = () => {
+    intervalRef.current = setInterval(async () => {
+      if (count < 20) {
+        setcount((count) => {
+          console.log('count', count + 1)
+          return count + 1
+        })
+      } else {
+        console.log('进度条20%')
+      }
+    }, 1000);
+  }
+  const handleBtn = () => {
+    if (!flag) {
+      setflag(true)
+      startatime(); // 开启定时器
+
+    } else {
+      console.log('点击终止导入');
+      setout(true)
+      stopatime()
+    }
   }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={handleBtn}>{flag ? '终止导入' : '导入'}</button>
+        <button>{count}</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
